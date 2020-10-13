d2b556513ce1f20c2691118510f2438bd8120128
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-13 14:00:32 -0400

    initialized Node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+class Node
+  attr_reader :value
+  attr_accessor :left, :right
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+end
